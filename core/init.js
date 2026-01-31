dc7e16dbf340a7d09f88773e208b4f67fd1de0e3
sl3pycat <sl3pycat@gmail.com>
2026-01-31 04:30:02 +0200

    added manifest and app ready event

--- core/init.js
+++ core/init.js
@@ -1,4 +1,27 @@
-/**@docs Core
-* App internals
-**/
+console.time("HyprJSON")
+window.hypr = {
+  //build version
+  version:0.0
+}
 
+/**@docs Core
+ * #### Manifest
+ * Add these to the hyprjson script tag:
+ * Key | Value | Description
+ * --- | --- | ---
+ * import | StringList | A space seperated list that imports script tags into the app.
+ * 
+ * example:
+ * ```html
+ * <script src="path/to/hyprjson.min.js" import="/index.js https://example.com/example.js"></script>
+ * ```
+ **/
+//get app manifest into a single object for convenience
+hypr.manifest = Object.fromEntries(Array.from(document.currentScript.attributes).map(attr => [attr.name, attr.value]))
+//imports scripts into the app
+if(hypr.manifest.import)hypr.manifest.import.split(" ").filter(entry=>entry).forEach(entry=>{
+  const script = document.createElement("script")
+  script.src = entry
+  script.async = false
+  document.head.appendChild(script)
+})
---
+++ core/ready.js
@@ -0,0 +1,4 @@
+window.onload = function(){
+  //bench how fast hypr initiated
+  console.timeEnd("HyprJSON")
+}
